3f4c09dae5af0f8f5af9e327d0c7d503e8ad37b5
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-12-22 21:25:45 -0200

    helpers folder living outside the projects

--- ra/lib/ra.rb
+++ ra/lib/ra.rb
@@ -1,6 +1,19 @@
 require 'ra/version'
+require 'colorize'
 
 program :name, 'ra'
 program :version, Ra::VERSION
 program :description, 'ra utility program.'
 helpers 'IO'
+
+TEMP = File.dirname(__FILE__) + '/../templates'
+
+command :new do
+  syntax 'ra new <program name>'  
+  description 'generates a default rails api program'
+  action do |args, options|
+    puts "generating rails program"
+    run_cmd "rails new #{args[0]} --api"
+    puts "command finished".colorize(:green)
+  end
+end
