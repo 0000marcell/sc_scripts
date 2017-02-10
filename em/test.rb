0a547815a3f91de1170fe12dd2c231ad832f3781
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-02-10 11:46:49 -0200

    in_file_snippet? IO method added

--- em/test.rb
+++ em/test.rb
@@ -1,9 +1,6 @@
-line = "this.post('/users').something"
-start_regexp = Regexp.new(Regexp.escape("this.post('/users')"))
-if line =~ start_regexp 
-  puts "match"
-end
+require 'byebug'
 
+name = "marcell"
+debugger
 
-
-
+value = "12"
